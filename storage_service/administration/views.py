8b1b5727bc9f0b16c996e94ae4e22ecd72506324
Holly Becker <hbecker@artefactual.com>
2013-08-14 10:54:15 -0700

    User passwords can be set from the edit user page
    refs #5260
    
    When editing a user, passwords can be set.  Admin users can edit anyone
    (and change their password), non-admin users can only change their own
    password.

--- storage_service/administration/views.py
+++ storage_service/administration/views.py
@@ -1,8 +1,9 @@
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import SetPasswordForm
 from django.shortcuts import render, redirect, get_object_or_404
 
-from . import forms
+from . import forms as settings_forms
 from . import models
 
 
@@ -10,7 +11,7 @@ from . import models
 
 def settings_edit(request):
     initial_data = dict(models.Settings.objects.all().values_list('name', 'value'))
-    form = forms.SettingsForm(request.POST or None, initial=initial_data)
+    form = settings_forms.SettingsForm(request.POST or None, initial=initial_data)
     if form.is_valid():
         # Save settings
         form.save()
@@ -27,16 +28,19 @@ def user_list(request):
 def user_edit(request, username):
     action = "Edit"
     edit_user = get_object_or_404(get_user_model(), username=username)
-    user_form = forms.UserChangeForm(request.POST or None, instance=edit_user,
-        prefix='user')
-    if user_form.is_valid():
+    user_form = settings_forms.UserChangeForm(request.POST or None, instance=edit_user)
+    password_form = SetPasswordForm(data=request.POST or None, user=edit_user)
+    if 'user' in request.POST and user_form.is_valid():
         user_form.save()
+        return redirect('user_list')
+    elif 'password' in request.POST and password_form.is_valid():
+        password_form.save()
         return redirect('user_list')
     return render(request, 'administration/user_form.html', locals())
 
 def user_create(request):
     action = "Create"
-    user_form = forms.UserCreationForm(request.POST or None, prefix="user")
+    user_form = settings_forms.UserCreationForm(request.POST or None)
     if user_form.is_valid():
         user_form.save()
         return redirect('user_list')
