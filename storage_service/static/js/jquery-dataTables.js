5c673c6142761391a927715bf268c5a7bdefa5f8
Holly Becker <hbecker@artefactual.com>
2013-08-16 14:54:58 -0700

    Renamed javascript so Github gets correct language
    Renamed jquery.dataTables.js to jquery-dataTables.js, so github ignores
    it in the language checking.  Updated templates for change.

--- storage_service/static/js/jquery-dataTables.js
+++ storage_service/static/js/jquery-dataTables.js
@@ -2,7 +2,7 @@
  * @summary     DataTables
  * @description Paginate, search and sort HTML tables
  * @version     1.9.4
- * @file        jquery.dataTables.js
+ * @file        jquery-dataTables.js
  * @author      Allan Jardine (www.sprymedia.co.uk)
  * @contact     www.sprymedia.co.uk/contact
  *
