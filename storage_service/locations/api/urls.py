1046af0d9915966e8e35036e8a7dad84aa1dd504
Misty De Meo <mdemeo@artefactual.com>
2013-12-16 15:35:43 -0800

    Send paths as base64 strings
    Paths are basically bytestrings to the OS, and we've caused ourselves
    a lot of grief by trying to treat them as sets of characters instead
    of bytes. Legacy data can, and frequently does, contain data in non-
    Unicode encodings, mixed encodings, invalid encodings, etc.
    
    v2 of the API sidesteps this by treating all paths as sets
    of arbitrary bytes, and communicates them to/from outside services in
    base64-encoded strings. The v1 API remains available and returns paths
    as strings like before.
    
    refs #6067

--- storage_service/locations/api/urls.py
+++ storage_service/locations/api/urls.py
@@ -1,15 +1,21 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
-from .resources import (LocationResource, SpaceResource, PackageResource,
-    PipelineResource)
+from locations.api import v1, v2
 
 v1_api = Api(api_name='v1')
-v1_api.register(SpaceResource())
-v1_api.register(LocationResource())
-v1_api.register(PackageResource())
-v1_api.register(PipelineResource())
+v1_api.register(v1.SpaceResource())
+v1_api.register(v1.LocationResource())
+v1_api.register(v1.PackageResource())
+v1_api.register(v1.PipelineResource())
 
+
+v2_api = Api(api_name='v2')
+v2_api.register(v2.SpaceResource())
+v2_api.register(v2.LocationResource())
+v2_api.register(v2.PackageResource())
+v2_api.register(v2.PipelineResource())
 
 urlpatterns = patterns('',
     (r'', include(v1_api.urls)),
+    (r'', include(v2_api.urls)),
 )
