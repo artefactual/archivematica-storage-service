7f968bd3d11cf293926a3c912da0193af6d8df70
Misty De Meo <mdemeo@artefactual.com>
2013-12-16 15:35:43 -0800

    Send paths as base64 strings
    Paths are basically bytestrings to the OS, and we've caused ourselves
    a lot of grief by trying to treat them as sets of characters instead
    of bytes. Legacy data can, and frequently does, contain data in non-
    Unicode encodings, mixed encodings, invalid encodings, etc.
    
    v2 of the API sidesteps this by treating all paths as sets
    of arbitrary bytes, and communicates them to/from outside services in
    base64-encoded strings. The v1 API remains available and returns paths
    as strings like before.
    
    refs #6067

--- storage_service/locations/api/resources.py
+++ storage_service/locations/api/resources.py
@@ -1,3 +1,6 @@
+# This file contains the base models that individual versioned models
+# are based on. They shouldn't be directly used with Api objects.
+
 # stdlib, alphabetical
 import json
 import logging
@@ -54,6 +57,7 @@ class PipelineResource(ModelResource):
         authorization = Authorization()
         # authorization = DjangoAuthorization()
         # validation = CleanedDataFormValidation(form_class=PipelineForm)
+        resource_name = 'pipeline'
 
         fields = ['uuid', 'description']
         list_allowed_methods = ['get', 'post']
@@ -83,6 +87,7 @@ class SpaceResource(ModelResource):
         authorization = Authorization()
         # authorization = DjangoAuthorization()
         validation = CleanedDataFormValidation(form_class=SpaceForm)
+        resource_name = 'space'
 
         fields = ['access_protocol', 'last_verified', 'location_set', 'path',
             'size', 'used', 'uuid', 'verified']
@@ -143,6 +148,10 @@ class SpaceResource(ModelResource):
         obj.save()
         return bundle
 
+    def get_objects(self, space, path):
+        message = 'This method should be accessed via a versioned subclass'
+        raise NotImplementedError(message)
+
     def browse(self, request, **kwargs):
         """ Returns all of the entries in a space, optionally at a subpath.
 
@@ -160,7 +169,7 @@ class SpaceResource(ModelResource):
         space = Space.objects.get(uuid=kwargs['uuid'])
         path = os.path.join(space.path, path)
 
-        objects = space.browse(path)
+        objects = self.get_objects(space, path)
 
         self.log_throttled_access(request)
         return self.create_response(request, objects)
@@ -187,6 +196,7 @@ class LocationResource(ModelResource):
         authorization = Authorization()
         # authorization = DjangoAuthorization()
         # validation = CleanedDataFormValidation(form_class=LocationForm)
+        resource_name = 'location'
 
         fields = ['enabled', 'relative_path', 'purpose', 'quota', 'used', 'uuid']
         list_allowed_methods = ['get']
@@ -211,6 +221,13 @@ class LocationResource(ModelResource):
             url(r"^(?P<resource_name>%s)/(?P<%s>\w[\w/-]*)/sword/state%s$" % (self._meta.resource_name, self._meta.detail_uri_name, trailing_slash()), self.wrap_view('sword_deposit_state'), name="sword_deposit_state"),
         ]
 
+    def decode_path(self, path):
+        return path
+
+    def get_objects(self, space, path):
+        message = 'This method should be accessed via a versioned subclass'
+        raise NotImplementedError(message)
+
     def browse(self, request, **kwargs):
         """ Returns all of the entries in a location, optionally at a subpath.
 
@@ -225,10 +242,11 @@ class LocationResource(ModelResource):
         self.is_authenticated(request)
         self.throttle_check(request)
         path = request.GET.get('path', '')
+        path = self.decode_path(path)
         location = Location.objects.get(uuid=kwargs['uuid'])
-        path = os.path.join(location.full_path(), path)
+        path = os.path.join(str(location.full_path()), path)
 
-        objects = location.space.browse(path)
+        objects = self.get_objects(location.space, path)
 
         self.log_throttled_access(request)
         return self.create_response(request, objects)
@@ -290,6 +308,7 @@ class PackageResource(ModelResource):
         authorization = Authorization()
         # authorization = DjangoAuthorization()
         # validation = CleanedDataFormValidation(form_class=PackageForm)
+        resource_name = 'package'
 
         fields = ['current_path', 'package_type', 'size', 'status', 'uuid']
         list_allowed_methods = ['get', 'post']
