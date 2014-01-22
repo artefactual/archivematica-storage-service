a43214de244c96676f6c8cd6636f21b98fd6853e
Holly Becker <hbecker@artefactual.com>
2014-01-22 11:34:38 -0800

    Initial LOCKSS tests
    Add pytest and pytest-django as testing dependencies.  Add testing fixtures
    - initial data needed for everything, and a lockss-o-matic specific one.
    Start on adding tests; currently only work if the modified simple sword
    server is running.

---
+++ storage_service/locations/tests/test_models.py
@@ -0,0 +1,30 @@
+
+from django.test import TestCase
+
+from locations import models
+
+
+class TestLockssomatic(TestCase):
+
+    fixtures = ['initial_data.json', 'lockssomatic.json']
+
+    def setUp(self):
+        self.lom_object = models.Lockssomatic.objects.all()[0]
+
+    def test_service_doc_bad_url(self):
+        self.lom_object.sd_iri = 'http://does-not-exist.com'
+        assert self.lom_object.update_service_document() == False
+        assert self.lom_object.au_size == 0
+        assert self.lom_object.collection_iri == None
+        assert self.lom_object.checksum_type == None
+
+    def test_get_service_doc(self):
+        # TODO mock response
+        assert self.lom_object.au_size == None
+        ret = self.lom_object.update_service_document()
+        assert ret == True
+        assert self.lom_object.au_size != 0
+        assert self.lom_object.sd_iri  # is positive
+        assert self.lom_object.collection_iri != ''
+        assert self.lom_object.checksum_type != None
+
