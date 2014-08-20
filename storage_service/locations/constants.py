3e72db2c628ce03a0753cba93b0a33d20043e118
Holly Becker <hbecker@artefactual.com>
2014-08-20 15:04:49 -0700

    Clean up constants.py
    Import models and forms, instead of individual classes. Move everything
    inside PROTOCOL definition.

--- storage_service/locations/constants.py
+++ storage_service/locations/constants.py
@@ -1,49 +1,47 @@
 
-from .models import LocalFilesystem, NFS, PipelineLocalFS, Lockssomatic, Fedora, Space
-from .forms import LocalFilesystemForm, NFSForm, PipelineLocalFSForm, LockssomaticForm, FedoraForm
-
+import forms
+import models
 
 ########################## SPACES ##########################
 
 # Mapping between access protocol and protocol specific fields
-PROTOCOL = {}
-# BUG: fields: [] works for obj_create, but includes everything in model_to_dict
-PROTOCOL[Space.LOCAL_FILESYSTEM] = {
-    'model': LocalFilesystem,
-    'form': LocalFilesystemForm,
-    'fields': []
-}
-PROTOCOL[Space.NFS] = {
-    'model': NFS,
-    'form': NFSForm,
-    'fields': ['manually_mounted',
-               'remote_name',
-               'remote_path',
-               'version']
-}
-PROTOCOL[Space.PIPELINE_LOCAL_FS] = {
-    'model': PipelineLocalFS,
-    'form': PipelineLocalFSForm,
-    'fields': ['remote_user',
-               'remote_name']
-}
-
-PROTOCOL[Space.LOM] = {
-    'model': Lockssomatic,
-    'form': LockssomaticForm,
-    'fields': ['au_size',
-               'sd_iri',
-               'collection_iri',
-               'content_provider_id',
-               'external_domain',
-               'keep_local',
-               'checksum_type',]
-}
-
-PROTOCOL[Space.FEDORA] = {
-    'model': Fedora,
-    'form': FedoraForm,
-    'fields': ['fedora_user',
-               'fedora_password',
-               'fedora_name']
+PROTOCOL = {
+    models.Space.FEDORA: {
+        'model': models.Fedora,
+        'form': forms.FedoraForm,
+        'fields': ['fedora_user',
+                   'fedora_password',
+                   'fedora_name']
+    },
+    # BUG: fields: [] works for obj_create, but includes everything in model_to_dict
+    models.Space.LOCAL_FILESYSTEM: {
+        'model': models.LocalFilesystem,
+        'form': forms.LocalFilesystemForm,
+        'fields': []
+    },
+    models.Space.LOM: {
+        'model': models.Lockssomatic,
+        'form': forms.LockssomaticForm,
+        'fields': ['au_size',
+                   'sd_iri',
+                   'collection_iri',
+                   'content_provider_id',
+                   'external_domain',
+                   'keep_local',
+                   'checksum_type']
+    },
+    models.Space.NFS: {
+        'model': models.NFS,
+        'form': forms.NFSForm,
+        'fields': ['manually_mounted',
+                   'remote_name',
+                   'remote_path',
+                   'version']
+    },
+    models.Space.PIPELINE_LOCAL_FS: {
+        'model': models.PipelineLocalFS,
+        'form': forms.PipelineLocalFSForm,
+        'fields': ['remote_user',
+                   'remote_name']
+    },
 }
