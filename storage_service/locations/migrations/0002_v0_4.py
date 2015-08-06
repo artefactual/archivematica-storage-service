ad019befa9b262fd2336972ef83e6a7850524c58
Holly Becker <hbecker@artefactual.com>
2015-08-06 16:17:38 -0700

    Migrations: Back-add migrations for 0.3 and later
    refs #7307
    
    To support upgrading from 0.3 and later, add Django migrations for each of
    those releases. Fake apply those migrations in postinst for more recent
    releases. All migrations for after 0.7 should be created solely as Django
    migrations

---
+++ storage_service/locations/migrations/0002_v0_4.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import jsonfield.fields
+import locations.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('locations', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Lockssomatic',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('au_size', models.BigIntegerField(help_text=b'Size in bytes of an Allocation Unit', null=True, verbose_name=b'AU Size', blank=True)),
+                ('sd_iri', models.URLField(help_text=b'URL of LOCKSS-o-matic service document IRI, eg. http://lockssomatic.example.org/api/sword/2.0/sd-iri', max_length=256, verbose_name=b'Service Document IRI')),
+                ('collection_iri', models.CharField(help_text=b'URL to post the packages to, eg. http://lockssomatic.example.org/api/sword/2.0/col-iri/12', max_length=256, null=True, verbose_name=b'Collection IRI', blank=True)),
+                ('content_provider_id', models.CharField(help_text=b'On-Behalf-Of value when communicating with LOCKSS-o-matic', max_length=32, verbose_name=b'Content Provider ID')),
+                ('external_domain', models.URLField(help_text=b'Base URL for this server that LOCKSS will be able to access.  Probably the URL for the home page of the Storage Service.', verbose_name=b'Externally available domain')),
+                ('checksum_type', models.CharField(help_text=b'Checksum type to send to LOCKSS-o-matic for verification.  Eg. md5, sha1, sha256', max_length=64, null=True, verbose_name=b'Checksum type', blank=True)),
+                ('keep_local', models.BooleanField(default=True, help_text=b'If checked, keep a local copy even after the AIP is stored in the LOCKSS network.', verbose_name=b'Keep local copy?')),
+                ('space', models.OneToOneField(to='locations.Space', to_field=b'uuid')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterModelOptions(
+            name='localfilesystem',
+            options={'verbose_name': 'Local Filesystem'},
+        ),
+        migrations.AlterModelOptions(
+            name='nfs',
+            options={'verbose_name': 'Network File System (NFS)'},
+        ),
+        migrations.AlterModelOptions(
+            name='pipelinelocalfs',
+            options={'verbose_name': 'Pipeline Local FS'},
+        ),
+        migrations.AddField(
+            model_name='package',
+            name='misc_attributes',
+            field=jsonfield.fields.JSONField(default={}, help_text=b'For storing flexible, often Space-specific, attributes', null=True, blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='space',
+            name='staging_path',
+            field=models.TextField(default='/var/archivematica/storage_service/', help_text=b'Absolute path to a staging area.  Must be UNIX filesystem compatible, preferably on the same filesystem as the path.', validators=[locations.models.space.validate_space_path]),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='location',
+            name='purpose',
+            field=models.CharField(help_text=b'Purpose of the space.  Eg. AIP storage, Transfer source', max_length=2, choices=[(b'TS', b'Transfer Source'), (b'AS', b'AIP Storage'), (b'DS', b'DIP Storage'), (b'BL', b'Transfer Backlog'), (b'CP', b'Currently Processing'), (b'SS', b'Storage Service Internal Processing')]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='package',
+            name='size',
+            field=models.IntegerField(default=0, help_text=b'Size in bytes of the package'),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='package',
+            name='status',
+            field=models.CharField(default=b'FAIL', help_text=b'Status of the package in the storage service.', max_length=8, choices=[(b'PENDING', b'Upload Pending'), (b'STAGING', b'Staged on Storage Service'), (b'UPLOADED', b'Uploaded'), (b'VERIFIED', b'Verified'), (b'FAIL', b'Failed'), (b'DEL_REQ', b'Delete requested'), (b'DELETED', b'Deleted')]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='space',
+            name='access_protocol',
+            field=models.CharField(help_text=b'How the space can be accessed.', max_length=8, choices=[(b'FS', b'Local Filesystem'), (b'NFS', b'NFS'), (b'PIPE_FS', b'Pipeline Local Filesystem'), (b'LOM', b'LOCKSS-o-matic')]),
+            preserve_default=True,
+        ),
+    ]
---
+++ storage_service/locations/migrations/0004_v0_7.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('locations', '0003_v0_5'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Arkivum',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('host', models.CharField(help_text=b'Hostname of the Arkivum web instance. Eg. arkivum.example.com:8443', max_length=256)),
+                ('remote_user', models.CharField(help_text=b'Optional: Username on the remote machine accessible via passwordless ssh.', max_length=64, null=True, blank=True)),
+                ('remote_name', models.CharField(help_text=b'Optional: Name or IP of the remote machine.', max_length=256, null=True, blank=True)),
+                ('space', models.OneToOneField(to='locations.Space', to_field=b'uuid')),
+            ],
+            options={
+                'verbose_name': 'Arkivum',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Swift',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('auth_url', models.CharField(help_text=b'URL to authenticate against', max_length=256)),
+                ('auth_version', models.CharField(default=b'2', help_text=b'OpenStack auth version', max_length=8)),
+                ('username', models.CharField(help_text=b'Username to authenticate as. E.g. http://example.com:5000/v2.0/', max_length=64)),
+                ('password', models.CharField(help_text=b'Password to authenticate with', max_length=256)),
+                ('container', models.CharField(max_length=64)),
+                ('tenant', models.CharField(help_text=b'The tenant/account name, required when connecting to an auth 2.0 system.', max_length=64, null=True, blank=True)),
+                ('region', models.CharField(help_text=b'Optional: Region in Swift', max_length=64, null=True, blank=True)),
+                ('space', models.OneToOneField(to='locations.Space', to_field=b'uuid')),
+            ],
+            options={
+                'verbose_name': 'Swift',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AlterModelOptions(
+            name='file',
+            options={'verbose_name': 'File'},
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='accessionid',
+            field=models.TextField(help_text=b'Accession ID of originating transfer', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='origin',
+            field=django_extensions.db.fields.UUIDField(default='', help_text=b'Unique identifier of originating Archivematica dashboard', max_length=36, editable=False, blank=True),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='package',
+            field=models.ForeignKey(to='locations.Package', null=True),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='file',
+            name='source_package',
+            field=models.TextField(help_text=b'Unique identifier of originating unit', blank=True),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='event_type',
+            field=models.CharField(max_length=8, choices=[(b'DELETE', b'delete'), (b'RECOVER', b'recover')]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='location',
+            name='purpose',
+            field=models.CharField(help_text=b'Purpose of the space.  Eg. AIP storage, Transfer source', max_length=2, choices=[(b'AR', b'AIP Recovery'), (b'AS', b'AIP Storage'), (b'CP', b'Currently Processing'), (b'DS', b'DIP Storage'), (b'SD', b'FEDORA Deposits'), (b'SS', b'Storage Service Internal Processing'), (b'BL', b'Transfer Backlog'), (b'TS', b'Transfer Source')]),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='space',
+            name='access_protocol',
+            field=models.CharField(help_text=b'How the space can be accessed.', max_length=8, choices=[(b'ARKIVUM', b'Arkivum'), (b'DC', b'DuraCloud'), (b'FEDORA', b'FEDORA via SWORD2'), (b'FS', b'Local Filesystem'), (b'LOM', b'LOCKSS-o-matic'), (b'NFS', b'NFS'), (b'PIPE_FS', b'Pipeline Local Filesystem'), (b'SWIFT', b'Swift')]),
+            preserve_default=True,
+        ),
+    ]
