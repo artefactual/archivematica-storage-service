b95803a8b58209a6547d4d45114ead73e0499e2c
Holly Becker <hbecker@artefactual.com>
2015-09-15 15:40:16 -0700

    Dataverse: Add browse
    Dataverse doesn't have a traditional directory structure, so browse is
    actually a query of datasets, with the location path contained in the query.

--- storage_service/locations/models/dataverse.py
+++ storage_service/locations/models/dataverse.py
@@ -1,15 +1,18 @@
 # stdlib, alphabetical
+import json
 import logging
 
 # Core Django, alphabetical
 from django.db import models
 
 # Third party dependencies, alphabetical
+import requests
 
 # This project, alphabetical
 LOGGER = logging.getLogger(__name__)
 
 # This module, alphabetical
+from . import StorageException
 from location import Location
 
 
@@ -19,6 +22,7 @@ class Dataverse(models.Model):
         help_text='Hostname of the Dataverse instance. Eg. apitest.dataverse.org')
     api_key = models.CharField(max_length=50,
         help_text='API key for Dataverse instance. Eg. b84d6b87-7b1e-4a30-a374-87191dbbbe2d')
+    # FIXME disallow string in space.path
 
     class Meta:
         verbose_name = "Dataverse"
@@ -29,7 +33,54 @@ class Dataverse(models.Model):
     ]
 
     def browse(self, path):
-        pass
+        """
+        Fetch a list of datasets from Dataverse based on the query in the location path.
+
+        Datasets are considered directories when browsing.
+        """
+        # Use http://guides.dataverse.org/en/latest/api/search.html to search and return datasets
+        # Location path is query string
+        # FIXME only browse one layer deep
+        url = 'https://' + self.host + '/api/search/'
+        params = {
+            'key': self.api_key,
+            'q': path,
+            'type': 'dataset',
+            'sort': 'name',
+            'order': 'asc',
+            'start': 0,
+            'per_page': 10,
+            'show_entity_ids': True,
+        }
+        entries = []
+        properties = {}
+        while True:
+            LOGGER.debug('URL: %s, params: %s', url, params)
+            response = requests.get(url, params=params)
+            LOGGER.debug('Response: %s', response)
+            if response.status_code != 200:
+                LOGGER.warning('%s: Response: %s', response, response.text)
+                raise StorageException('Unable to fetch datasets from %s with query %s' % (url, path))
+            try:
+                data = response.json()['data']
+            except json.JSONDecodeError:
+                LOGGER.error('Could not parse JSON from response to %s', url)
+                raise StorageException('Unable parse JSON from response to %s with query %s' % (url, path))
+
+            # WARNING 'name' is not unique - may generate incorrect properties
+            entries += [x['name'] for x in data['items']]
+
+            if params['start'] + data['count_in_response'] < data['total_count']:
+                params['start'] += data['count_in_response']
+            else:
+                break
+
+        directories = entries
+        return {
+            'directories': directories,
+            'entries': entries,
+            'properties': properties,
+        }
 
     def move_to_storage_service(self, src_path, dest_path, dest_space):
         """ Moves src_path to dest_space.staging_path/dest_path. """
--- storage_service/locations/tests/test_dataverse.py
+++ storage_service/locations/tests/test_dataverse.py
@@ -14,7 +14,39 @@ class TestDataverse(TestCase):
 
     def setUp(self):
         self.dataverse = models.Dataverse.objects.all()[0]
+        self.dataverse_location = models.Location.objects.get(space=self.dataverse.space)
 
     def test_has_required_attributes(self):
         assert self.dataverse.host
         assert self.dataverse.api_key
+
+    @vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dataverse_browse_all.yaml'))
+    def test_browse_all(self):
+        """
+        It should fetch a list of datasets.
+        It should handle iteration.
+        """
+        resp = self.dataverse.browse('*')
+        assert len(resp['directories']) == 15
+        assert len(resp['entries']) == 15
+        assert resp['entries'] == resp['directories']
+        assert resp['entries'][0] == 'Ad hoc observational study of the trees outside my window'
+        assert resp['entries'][1] == 'Constitive leaf ORAC'
+        assert resp['entries'][14] == 'testjpg'
+        assert len(resp['properties']) == 0
+
+    @vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dataverse_browse_filter.yaml'))
+    def test_browse_filter(self):
+        """
+        It should fetch a list of datasets.
+        It should filter based on the provided query
+        """
+        resp = self.dataverse.browse('title:test*')
+        assert len(resp['directories']) == 4
+        assert len(resp['entries']) == 4
+        assert resp['entries'] == resp['directories']
+        assert resp['entries'][0] == 'Metadata mapping test study'
+        assert resp['entries'][1] == 'Restricted Studies Test'
+        assert resp['entries'][2] == 'testdocx'
+        assert resp['entries'][3] == 'testjpg'
+        assert len(resp['properties']) == 0
