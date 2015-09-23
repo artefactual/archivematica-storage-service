503661cda58f04035e15021cd285333e67f44a71
Mike Cantelon <mcantelon@gmail.com>
2015-09-23 15:40:57 -0700

    Log fixity check results
    Where endpoint for fixity checks is requested, log the results of the check.

--- storage_service/locations/signals.py
+++ storage_service/locations/signals.py
@@ -1,12 +1,16 @@
+import json
 import logging
 from django.dispatch import receiver, Signal
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
+import models
+
 LOGGER = logging.getLogger(__name__)
 
 deletion_request = Signal(providing_args=["uuid", "location", "url"])
 failed_fixity_check = Signal(providing_args=["uuid", "location", "report"])
+successful_fixity_check = Signal(providing_args=["uuid", "location", "report"])
 
 
 def _notify_administrators(subject, message):
@@ -35,8 +39,16 @@ To approve this deletion request, visit: {}{}
     _notify_administrators(subject, message)
 
 
+def _log_report(uuid, success, message=None):
+    package = models.Package.objects.get(uuid=uuid)
+    models.FixityLog.objects.create(package=package, success=success, error_details=message)
+
+
 @receiver(failed_fixity_check, dispatch_uid="fixity_check")
 def report_failed_fixity_check(sender, **kwargs):
+    report_data = json.loads(kwargs['report'])
+    _log_report(kwargs['uuid'], False, report_data['message'])
+
     subject = "Fixity check failed for package {}".format(kwargs["uuid"])
     message = """
 A fixity check failed for the package with UUID {}. This package is currently stored at: {}
@@ -44,4 +56,10 @@ A fixity check failed for the package with UUID {}. This package is currently st
 Full failure report (in JSON format):
 {}
 """.format(kwargs["uuid"], kwargs["location"], kwargs["report"])
+
     _notify_administrators(subject, message)
+
+
+@receiver(successful_fixity_check, dispatch_uid="fixity_check")
+def report_successful_fixity_check(sender, **kwargs):
+    _log_report(kwargs['uuid'], True)
