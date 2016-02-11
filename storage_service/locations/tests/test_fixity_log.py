fd2e84a3256672793acde98f6c89e034e0f56ff0
Mike Cantelon <mcantelon@gmail.com>
2016-02-11 14:19:10 -0800

    Fixity logging
    Squashed commit of the following:
    
    commit 318d129bf8392e48f711731ede19c720de6de631
    Author: Mike Cantelon <mcantelon@gmail.com>
    Date:   Tue Dec 15 12:01:07 2015 -0800
    
        Added fixity log tests.
    
    commit 298f80094b6d1413798e09e15e4c24973ff72151
    Author: Mike Cantelon <mcantelon@gmail.com>
    Date:   Wed Sep 23 15:45:08 2015 -0700
    
        Added fixity status/log display
    
        Added display of the last fixity check date/result in the package list page.
        Each result links to a page showing a log of fixity checks on the package.
    
    commit b2ddd7ffb00b7b288f8aa0abc7e50805638e1491
    Author: Mike Cantelon <mcantelon@gmail.com>
    Date:   Wed Sep 23 15:40:57 2015 -0700
    
        Log fixity check results
    
        Where endpoint for fixity checks is requested, log the results of the check.
    
    commit dd9b7471c9299ff9a74ce1fb6ac78b4f84359996
    Author: Mike Cantelon <mcantelon@gmail.com>
    Date:   Wed Sep 23 15:38:24 2015 -0700
    
        Add fixity log entry model.
    
        This model will be used to support logging of fixity check results.

---
+++ storage_service/locations/tests/test_fixity_log.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from locations import models
+
+
+class TestFixityLog(TestCase):
+
+    fixtures = ['base.json', 'fixity_log.json']
+
+    def setUp(self):
+        self.fl_object = models.FixityLog.objects.all()[0]
+        #self.auth = requests.auth.HTTPBasicAuth(self.ds_object.user, self.ds_object.password)
+
+    def test_has_required_attributes(self):
+        assert self.fl_object.package
+        assert self.fl_object.success
+        assert self.fl_object.error_details
+        assert self.fl_object.datetime_reported
