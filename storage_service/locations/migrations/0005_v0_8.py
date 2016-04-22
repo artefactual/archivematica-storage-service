18ab9b8211f20eed1967c52a9b0497e1432e3b05
Holly Becker <hbecker@artefactual.com>
2016-04-21 17:32:16 -0700

    Migrations: Update for location<->pipeline model change

---
+++ storage_service/locations/migrations/0005_v0_8.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('locations', '0004_v0_7'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='location',
+            name='pipeline',
+            field=models.ManyToManyField(help_text=b'UUID of the Archivematica instance using this location.', to='locations.Pipeline', through='locations.LocationPipeline', blank=True),
+        ),
+    ]
