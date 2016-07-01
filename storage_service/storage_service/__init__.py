dfa27d573c3b7faf8dd9846b2227e9d2fce9a83f
Mike Cantelon <mcantelon@gmail.com>
2016-07-01 21:01:43 +0000

    Add Celery for async move.

--- storage_service/storage_service/__init__.py
+++ storage_service/storage_service/__init__.py
@@ -1,1 +1,7 @@
+from __future__ import absolute_import
+
+# This will make sure the app is always imported when
+# Django starts so that shared_task will use this app.
+from .celery import app as celery_app
+
 __version__ = '0.9.1'
