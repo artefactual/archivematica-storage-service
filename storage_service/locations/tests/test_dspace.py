59290ccacaeb6e246d369b1ab2bc48cb4f5dbb76
Holly Becker <hbecker@artefactual.com>
2016-07-11 17:03:16 -0700

    DSpace: Rough out move_from_storage_service

--- storage_service/locations/tests/test_dspace.py
+++ storage_service/locations/tests/test_dspace.py
@@ -18,7 +18,13 @@ class TestDSpace(TestCase):
     fixtures = ['base.json', 'dspace.json']
 
     def setUp(self):
-        self.dspace_object = models.DSpace.objects.all()[0]
+        self.dspace_object = models.DSpace.objects.get(id=1)
+
+    def tearDown(self):
+        try:
+            os.remove(os.path.join(FIXTURES_DIR, 'test.txt'))
+        except OSError:
+            pass
 
     def test_has_required_attributes(self):
         assert self.dspace_object.sd_iri
@@ -44,7 +50,18 @@ class TestDSpace(TestCase):
 
     @dspace_vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dspace_move_from_ss.yaml'))
     def test_move_from_ss(self):
-        pass
+        # Create test.txt
+        with open(os.path.join(FIXTURES_DIR, 'test.txt'), 'w') as f:
+            f.write('test file\n')
+        package = models.Package.objects.get(uuid='1056123d-8a16-49c2-ac51-8e5fa367d8b5')
+
+        # Upload
+        self.dspace_object.move_from_storage_service(os.path.join(FIXTURES_DIR, 'test.txt'), 'irrelevent', package=package)
+
+        # Verify
+        assert package.current_path == 'http://demo.dspace.org/swordv2/statement/86.atom'
+        assert package.misc_attributes['handle'] == '123456789/35'
+        # FIXME How to verify?
 
     @dspace_vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dspace_move_to_ss.yaml'))
     def test_move_to_ss(self):
