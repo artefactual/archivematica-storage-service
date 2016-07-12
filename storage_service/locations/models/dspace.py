ba5310562e854418104f3d6506c2985f686874eb
Holly Becker <hbecker@artefactual.com>
2016-07-11 17:03:16 -0700

    DSpace: Rough out move_from_storage_service

--- storage_service/locations/models/dspace.py
+++ storage_service/locations/models/dspace.py
@@ -7,11 +7,17 @@ IRI.
 from __future__ import absolute_import
 # stdlib, alphabetical
 import logging
+import mimetypes
+import os
+import re
+import urlparse
+import urllib
 
 # Core Django, alphabetical
 from django.db import models
 
 # Third party dependencies, alphabetical
+import requests
 import sword2
 
 # This project, alphabetical
@@ -69,6 +75,134 @@ class DSpace(models.Model):
         """ Moves src_path to dest_space.staging_path/dest_path. """
         pass
 
+    def _split_package(self, input_path):
+        """
+        Splits the input package into objects and metadata & logs.
+
+        :param str input_path: Path to the input AIP
+        :return: List of packages to be stored
+        """
+
+        return [input_path]
+
     def move_from_storage_service(self, source_path, destination_path, package=None):
-        """ Moves self.staging_path/src_path to dest_path. """
-        pass
+        LOGGER.info('source_path: %s, destination_path: %s, package: %s', source_path, destination_path, package)
+        if package is None:
+            LOGGER.warning('DSpace requires package param')
+            return
+
+        self._get_sword_connection()
+        # Create item by depositing AtoM doc
+        # TODO get DC info from METS?
+        LOGGER.debug('Create SWORD2 entry')
+        entry = sword2.Entry(
+            title=os.path.basename(source_path),
+            id='archivematica:id:42',  # TODO replace this
+            author={'name': 'archivematica'},  # TODO replace this
+        )
+
+        destination_path = package.current_location.relative_path
+        LOGGER.debug('POST SWORD2 entry %s %s', destination_path, entry)
+        entry_receipt = self.sword_connection.create(
+            col_iri=destination_path,
+            in_progress=True,
+            metadata_entry=entry,
+        )
+
+        # TODO store these in Package.misc_attributes
+        LOGGER.info('Edit IRI: %s', entry_receipt.edit)
+        LOGGER.info('Edit Media IRI: %s', entry_receipt.edit_media)
+        LOGGER.info('Statement IRI: %s', entry_receipt.atom_statement_iri)
+
+        # Split package
+        upload_paths = self._split_package(source_path)
+
+        for upload_path in upload_paths:
+            LOGGER.info('Add file %s to %s', upload_path, entry_receipt.edit_media)
+            # Add file to DSpace item
+            with open(upload_path, 'r') as f:
+                content = f.read()  # sword2 iterates over this twice
+
+            # Note: This has problems because httplib2 tries all requests using basic auth without any auth and retries after getting a 401. This breaks with files over 2097152 bytes.
+            # A possible solution is to use a different http_impl in the connection, but that returns incorrect URIs in the deposit recept
+            # LOGGER.debug('Using sword2')
+            # self.sword_connection.add_file_to_resource(
+            #     edit_media_iri=entry_receipt.edit_media,
+            #     payload=content,
+            #     filename=os.path.basename(upload_path),
+            #     mimetype=mimetypes.guess_type(upload_path),
+            # )
+
+            # This replicates the sword2 behaviour but using requests for the basic auth
+            LOGGER.debug('Using requests')
+            headers = {
+                'Content-Type': str(mimetypes.guess_type(upload_path)),
+                # 'Content-MD5': str(md5sum),
+                'Content-Length': str(os.path.getsize(upload_path)),
+                'Content-Disposition': "attachment; filename=%s" % urllib.quote(os.path.basename(upload_path)),
+            }
+            requests.post(entry_receipt.edit_media, headers=headers, data=content, auth=(self.user, self.password))
+
+        # Finalize deposit
+        LOGGER.info('Complete deposit for %s', entry_receipt.edit)
+        try:
+            complete_receipt = self.sword_connection.complete_deposit(dr=entry_receipt)
+        except Exception:
+            LOGGER.error('Error creating item: Status: %s, response: %s', self.sword_connection.history[-1]['payload']['response'].status, self.sword_connection.history[-1]['payload']['response'].resp)
+            LOGGER.error(self.sword_connection.history[-1])
+            raise
+        LOGGER.info('Complete receipt: %s', complete_receipt)
+
+        package.current_path = entry_receipt.atom_statement_iri
+        package.save()
+
+        # Fetch statement
+        LOGGER.info('Request Atom serialisation of the deposit statement from %s', entry_receipt.atom_statement_iri)
+        try:
+            statement = self.sword_connection.get_atom_sword_statement(entry_receipt.atom_statement_iri)
+        except Exception:
+            LOGGER.error('Error creating item: Status: %s, response: %s', self.sword_connection.history[-1]['payload']['response'].status, self.sword_connection.history[-1]['payload']['response'].resp)
+            LOGGER.error(self.sword_connection.history[-1])
+            raise
+        LOGGER.info('Statement: %s', statement.xml_document)
+
+        # Get DSpace handle
+        regex = r'bitstream/(?P<handle>\d+/\d+)/'  # get Dspace handle regex
+        match = re.search(regex, statement.original_deposits[0].id)
+        if match:
+            LOGGER.info('Handle: %s', match.group('handle'))
+            handle = match.group('handle')
+        else:
+            LOGGER.warning('No match found in %s', statement.original_deposits[0].id)
+            return
+
+        package.misc_attributes.update({'handle': handle})
+        package.save()
+
+        # Set bitstream permissions for bitstreams attached to handle
+        parsed_url = urlparse.urlparse(self.sd_iri)
+        dspace_url = urlparse.urlunparse((parsed_url.scheme, parsed_url.netloc, '', '', '', ''))
+        url = dspace_url + '/rest/handle/' + handle
+        headers = {'Accept': 'application/json'}
+        params = {'expand': 'bitstreams'}
+        response = requests.get(url, headers=headers, params=params)
+        LOGGER.info('REST API handle mapping %s %s', response.status_code, response)
+        LOGGER.info('Body %s', response.json())
+        for bitstream in response.json()['bitstreams']:
+            url = dspace_url + bitstream['link'] + '/policy'
+            LOGGER.info('Bitstream policy URL %s', url)
+            # # TODO what should a ResourcePolicy look like?
+            # body = [{
+            #     "action": "READ",
+            #     "epersonId": -1,
+            #     "groupId": 0,
+            #     "resourceId": 47166,
+            #     "resourceType": "bitstream",
+            #     "rpDescription": None,
+            #     "rpName": None,
+            #     "rpType": "TYPE_INHERITED",
+            #     "startDate": None,
+            #     "endDate": None
+            # }]
+
+            # requests.post(url, headers=headers, data=body)
