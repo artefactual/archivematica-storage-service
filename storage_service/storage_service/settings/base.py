ed50473f8c1b40312f516d59796bef737a202f04
Holly Becker <hbecker@artefactual.com>
2016-12-06 15:01:00 -0800

    Settings: Set ALLOWED_HOSTS in base.py

--- storage_service/storage_service/settings/base.py
+++ storage_service/storage_service/settings/base.py
@@ -118,7 +118,7 @@ SECRET_KEY = get_env_variable('DJANGO_SECRET_KEY')
 ########## SITE CONFIGURATION
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 ########## END SITE CONFIGURATION
 
 
