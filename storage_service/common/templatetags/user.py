86e279a26bf5dabb1e9d5fddf53dfc01727c6e77
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2017-06-09 14:36:05 +0000

    Make shibboleth logout link optional

---
+++ storage_service/common/templatetags/user.py
@@ -0,0 +1,13 @@
+from django import template
+from django.core.urlresolvers import reverse
+
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def logout_link(context):
+    if context.get('logout_link'):
+        return context['logout_link']
+    else:
+        return reverse('logout')
