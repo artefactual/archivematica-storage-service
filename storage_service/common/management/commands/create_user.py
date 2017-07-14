6cfe446fe4ec1c7ea3c61fbab541a0c1ce812fdb
Richard Lowe <richard.lowe@arkivum.com>
2017-07-14 13:48:26 +0100

    Create user: allow non-superuser user to be created
    This uses the `--superuser` flag.

--- storage_service/common/management/commands/create_user.py
+++ storage_service/common/management/commands/create_user.py
@@ -10,13 +10,14 @@ from tastypie.models import ApiKey
 class Command(BaseCommand):
     help = 'Create user with all the arguments specified. ' \
         'If the user already exists its attributes will be updated.'
-    args = ('username', 'email', 'password', 'api_key')
+    args = ('username', 'email', 'password', 'api_key', 'superuser')
 
     def add_arguments(self, parser):
         parser.add_argument('--username', required=True)
         parser.add_argument('--email', required=True)
         parser.add_argument('--password', required=True)
         parser.add_argument('--api-key', required=True)
+        parser.add_argument('--superuser', action='store_true', default=False)
 
     def handle(self, *args, **options):
         UserModel = get_user_model()
@@ -25,7 +26,12 @@ class Command(BaseCommand):
             user = UserModel._default_manager.get(**{UserModel.USERNAME_FIELD: options['username']})
             print('User found.')
         except UserModel.DoesNotExist:
-            user = UserModel._default_manager.create_superuser(options['username'], options['email'], options['password'])
+            if options['superuser']:
+                user = UserModel._default_manager.create_superuser(
+                    options['username'], options['email'], options['password'])
+            else:
+                user = UserModel._default_manager.create_user(
+                    options['username'], options['email'], options['password'])
             print('User could not be found, one was created.')
 
         if user is None:
