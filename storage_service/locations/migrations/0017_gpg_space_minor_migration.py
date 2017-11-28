7f49a34e605d701168ee88c8cff0e3b8ed9a68d6
Joel Dunham <jrwdunham@gmail.com>
2017-11-27 17:37:37 -0800

    Add GPG-related minor migration

---
+++ storage_service/locations/migrations/0017_gpg_space_minor_migration.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('locations', '0016_mirror_location_aip_replication'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='space',
+            name='access_protocol',
+            field=models.CharField(
+                help_text='How the space can be accessed.',
+                max_length=8,
+                verbose_name='Access protocol',
+                choices=[
+                    (b'ARKIVUM', 'Arkivum'),
+                    (b'DV', 'Dataverse'),
+                    (b'DC', 'DuraCloud'),
+                    (b'DSPACE', 'DSpace via SWORD2 API'),
+                    (b'FEDORA', 'FEDORA via SWORD2'),
+                    (b'GPG', 'GPG encryption on Local Filesystem'),
+                    (b'FS', 'Local Filesystem'),
+                    (b'LOM', 'LOCKSS-o-matic'),
+                    (b'NFS', 'NFS'),
+                    (b'PIPE_FS', 'Pipeline Local Filesystem'),
+                    (b'SWIFT', 'Swift')]),
+        ),
+    ]
