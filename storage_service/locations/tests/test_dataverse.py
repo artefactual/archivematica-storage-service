7b94630bc63036eceab8c19ee39b59ea1201dc8f
Joel Dunham <jrwdunham@gmail.com>
2018-07-05 12:00:07 -0700

    Fix dataverse space browse tests

--- storage_service/locations/tests/test_dataverse.py
+++ storage_service/locations/tests/test_dataverse.py
@@ -12,7 +12,7 @@ FIXTURES_DIR = os.path.abspath(os.path.join(THIS_DIR, '..', 'fixtures'))
 
 class TestDataverse(TestCase):
 
-    fixtures = ['base.json', 'dataverse.json']
+    fixtures = ['base.json', 'dataverse.json', 'dataverse2.json']
 
     def setUp(self):
         self.dataverse = models.Dataverse.objects.all()[0]
@@ -50,25 +50,48 @@ class TestDataverse(TestCase):
         assert resp['properties']['25']['verbose name'] == 'Constitive leaf ORAC'
         assert resp['properties']['14']['verbose name'] == 'testjpg'
 
-    @vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dataverse_browse_filter.yaml'))
-    def test_browse_filter(self):
+    @vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes',
+                      'dataverse_browse_filter.yaml'))
+    def test_browse_datasets(self):
         """
         It should fetch a list of datasets.
-        It should filter based on the provided query
+        It should fetch a list of objects within a dataset.
         """
-        resp = self.dataverse.browse('title:test*')
-        assert len(resp['directories']) == 4
-        assert len(resp['entries']) == 4
+        dataverse = models.Dataverse.objects.get(
+            agent_name='Archivematica Test Dataverse')
+        location = dataverse.space.location_set.get(purpose='TS')
+
+        # Get all datasets in a location
+        resp = dataverse.browse(location.relative_path)
+        assert len(resp['directories']) == 59
+        assert len(resp['entries']) == 59
         assert resp['entries'] == resp['directories']
-        assert resp['entries'][0] == '90'
-        assert resp['entries'][1] == '93'
-        assert resp['entries'][2] == '16'
-        assert resp['entries'][3] == '14'
-        assert len(resp['properties']) == 4
-        assert resp['properties']['90']['verbose name'] == 'Metadata mapping test study'
-        assert resp['properties']['93']['verbose name'] == 'Restricted Studies Test'
-        assert resp['properties']['16']['verbose name'] == 'testdocx'
-        assert resp['properties']['14']['verbose name'] == 'testjpg'
+        assert resp['entries'][0] == '1016'
+        assert resp['entries'][1] == '574'
+        assert resp['entries'][2] == '577'
+        assert resp['entries'][3] == '581'
+        assert len(resp['properties']) == 59
+        assert resp['properties']['1016']['verbose name'] == (
+            '3D Laser Images of a road cut at Ivy Lea, Ontario (2007),'
+            ' underground in Sudbury, Ontario (2007), underground in Thompson,'
+            ' Manitoba (2009) [test]')
+        assert resp['properties']['574']['verbose name'] == (
+            'A study of my afternoon drinks ')
+        assert resp['properties']['577']['verbose name'] == (
+            'A study with restricted data')
+        assert resp['properties']['581']['verbose name'] == (
+            'A sub-dataverse dataset')
+
+        # Get all objects in dataset 1016
+        resp = dataverse.browse('{}/1016'.format(location.relative_path))
+        assert resp['directories'] == []
+        assert len(resp['entries']) == 55
+        first4 = ['IvyLea_003.txt', 'IvyLea_004.txt', 'IvyLea_005.txt',
+                  'IvyLea_006.txt']
+        first4sizes = [47448936, 46736074, 47463997, 47723143]
+        assert first4 == resp['entries'][:4]
+        for idx, obj in enumerate(first4):
+            assert resp['properties'][obj]['size'] == first4sizes[idx]
 
     @vcr.use_cassette(os.path.join(FIXTURES_DIR, 'vcr_cassettes', 'dataverse_move_to.yaml'))
     def test_move_to(self):
