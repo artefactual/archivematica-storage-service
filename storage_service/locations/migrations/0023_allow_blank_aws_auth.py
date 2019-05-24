088b80583a373896006cc44d942e387836ac1a10
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-05-24 10:29:43 +0000

    Allow S3 auth details to be blank

---
+++ storage_service/locations/migrations/0023_allow_blank_aws_auth.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("locations", "0022_update_pipeline_help_text")]
+
+    operations = [
+        migrations.AlterField(
+            model_name='s3',
+            name='access_key_id',
+            field=models.CharField(max_length=64, verbose_name='Access Key ID to authenticate', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='s3',
+            name='secret_access_key',
+            field=models.CharField(max_length=256, verbose_name='Secret Access Key to authenticate with', blank=True),
+        ),
+    ]
--- storage_service/locations/models/s3.py
+++ storage_service/locations/models/s3.py
@@ -11,7 +11,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 # Third party dependencies, alphabetical
-import boto3
+import boto3, botocore
 import re
 
 # This project, alphabetical
@@ -31,10 +31,14 @@ class S3(models.Model):
         help_text=_("S3 Endpoint URL. Eg. https://s3.amazonaws.com"),
     )
     access_key_id = models.CharField(
-        max_length=64, verbose_name=_("Access Key ID to authenticate")
+        max_length=64,
+        blank=True,
+        verbose_name=_("Access Key ID to authenticate"),
     )
     secret_access_key = models.CharField(
-        max_length=256, verbose_name=_("Secret Access Key to authenticate with")
+        max_length=256,
+        blank=True,
+        verbose_name=_("Secret Access Key to authenticate with"),
     )
     region = models.CharField(
         max_length=64,
@@ -48,33 +52,23 @@ class S3(models.Model):
 
     ALLOWED_LOCATION_PURPOSE = [Location.AIP_STORAGE, Location.REPLICATOR]
 
-    def __init__(self, *args, **kwargs):
-        super(S3, self).__init__(*args, **kwargs)
-        self._client = None
-        self._resource = None
-
-    @property
-    def client(self):
-        if self._client is None:
-            self._client = boto3.client(
-                service_name="s3",
-                endpoint_url=self.endpoint_url,
-                aws_access_key_id=self.access_key_id,
-                aws_secret_access_key=self.secret_access_key,
-                region_name=self.region,
-            )
-        return self._client
 
     @property
     def resource(self):
-        if self._resource is None:
-            self._resource = boto3.resource(
-                service_name="s3",
-                endpoint_url=self.endpoint_url,
-                aws_access_key_id=self.access_key_id,
-                aws_secret_access_key=self.secret_access_key,
-                region_name=self.region,
-            )
+        if not hasattr(self, "_resource"):
+            boto_args = {
+                "service_name": "s3",
+                "endpoint_url": self.endpoint_url,
+                "region_name": self.region,
+            }
+            if self.access_key_id and self.secret_access_key:
+                boto_args.update(
+                    aws_access_key_id=self.access_key_id,
+                    aws_secret_access_key=self.secret_access_key,
+                )
+
+            self._resource = boto3.resource(**boto_args)
+
         return self._resource
 
     def _ensure_bucket_exists(self):
@@ -93,14 +87,14 @@ class S3(models.Model):
         """
         LOGGER.debug("Test the S3 bucket '%s' exists", self._bucket_name())
         try:
-            loc_info = self.client.get_bucket_location(Bucket=self._bucket_name())
+            loc_info = self.resource.get_bucket_location(Bucket=self._bucket_name())
             LOGGER.debug("S3 bucket's response: %s", loc_info)
-        except self.client.exceptions.ClientError as err:
+        except botocore.exceptions.ClientError as err:
             error_code = err.response["Error"]["Code"]
             if error_code != "NoSuchBucket":
                 raise StorageException(err)
             LOGGER.info("Creating S3 bucket '%s'", self._bucket_name())
-            self.client.create_bucket(
+            self.resource.create_bucket(
                 Bucket=self._bucket_name(),
                 CreateBucketConfiguration={"LocationConstraint": self.region},
             )
