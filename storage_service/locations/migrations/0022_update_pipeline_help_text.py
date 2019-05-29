33ab1657123a8ed84dee490f88417d0ec45dbeee
Héctor Akamine <hakamine@artefactual.com>
2019-05-28 21:29:14 -0700

    pipeline: fix remote_name help text

---
+++ storage_service/locations/migrations/0022_update_pipeline_help_text.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [("locations", "0021_alter_callback")]
+
+    operations = [
+        migrations.AlterField(
+            model_name="pipeline",
+            name="remote_name",
+            field=models.CharField(
+                default=None,
+                max_length=256,
+                blank=True,
+                help_text="Base URL of the pipeline server for making API calls.",
+                null=True,
+                verbose_name="Remote name",
+            ),
+        )
+    ]
