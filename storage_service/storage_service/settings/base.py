89a931b939f70d84e2dca04a81e18bf63f5343a2
Cole Maclean <hi@cole.io>
2019-07-15 12:08:17 -0700

    Expose LDAP TLS options via env variables
    Also, fix group cache timeout setting and document LDAP options
    in README

--- storage_service/storage_service/settings/base.py
+++ storage_service/storage_service/settings/base.py
@@ -380,14 +380,52 @@ if LDAP_AUTHENTICATION:
     )
     AUTH_LDAP_CACHE_GROUPS = is_true(environ.get("AUTH_LDAP_CACHE_GROUPS", "FALSE"))
     try:
-        AUTH_LDAP_GROUP_CACHE_TIMEOUT = int(environ.get("AUTH_LDAP_DENY_GROUP", ""))
+        AUTH_LDAP_GROUP_CACHE_TIMEOUT = int(
+            environ.get("AUTH_LDAP_GROUP_CACHE_TIMEOUT", "3600")
+        )
     except ValueError:
-        AUTH_LDAP_GROUP_CACHE_TIMEOUT = None
+        AUTH_LDAP_GROUP_CACHE_TIMEOUT = 3600
 
-    # Non-configurable sane defaults
-    AUTH_LDAP_START_TLS = True
-    AUTH_LDAP_ALWAYS_UPDATE_USER = True
+    AUTH_LDAP_START_TLS = is_true(environ.get("AUTH_LDAP_START_TLS", "TRUE"))
+
     AUTH_LDAP_GLOBAL_OPTIONS = {}
+    if environ.get("AUTH_LDAP_PROTOCOL_VERSION", None) == "3":
+        AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_PROTOCOL_VERSION] = ldap.VERSION3
+    if environ.get("AUTH_LDAP_TLS_CACERTFILE", None):
+        AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_CACERTFILE] = environ.get(
+            "AUTH_LDAP_TLS_CACERTFILE"
+        )
+    if environ.get("AUTH_LDAP_TLS_CERTFILE", None):
+        AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_CERTFILE] = environ.get(
+            "AUTH_LDAP_TLS_CERTFILE"
+        )
+    if environ.get("AUTH_LDAP_TLS_KEYFILE", None):
+        AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_KEYFILE] = environ.get(
+            "AUTH_LDAP_TLS_KEYFILE"
+        )
+    if environ.get("AUTH_LDAP_TLS_REQUIRE_CERT", None):
+        require_cert = environ.get("AUTH_LDAP_TLS_REQUIRE_CERT").lower()
+        if require_cert == "never":
+            AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_REQUIRE_CERT] = ldap.OPT_X_TLS_NEVER
+        elif require_cert == "allow":
+            AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_REQUIRE_CERT] = ldap.OPT_X_TLS_ALLOW
+        elif require_cert == "try":
+            AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_REQUIRE_CERT] = ldap.OPT_X_TLS_TRY
+        elif require_cert == "demand":
+            AUTH_LDAP_GLOBAL_OPTIONS[
+                ldap.OPT_X_TLS_REQUIRE_CERT
+            ] = ldap.OPT_X_TLS_DEMAND
+        elif require_cert == "hard":
+            AUTH_LDAP_GLOBAL_OPTIONS[ldap.OPT_X_TLS_REQUIRE_CERT] = ldap.OPT_X_TLS_HARD
+        else:
+            raise ImproperlyConfigured(
+                (
+                    "Unexpected value for AUTH_LDAP_TLS_REQUIRE_CERT: {}. "
+                    "Supported values: 'never', 'allow', try', 'hard', or 'demand'."
+                ).format(require_cert)
+            )
+    # Non-configurable sane defaults
+    AUTH_LDAP_ALWAYS_UPDATE_USER = True
 
 ######### END LDAP CONFIGURATION #########
 
