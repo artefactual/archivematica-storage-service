a10f0a7195e0d7be60871907ea5398dd0abc984b
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2019-08-01 13:34:18 -0600

    Fix common utilities tests

--- storage_service/common/utils.py
+++ storage_service/common/utils.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+from __future__ import unicode_literals
 import ast
 from collections import namedtuple
 import datetime
@@ -434,6 +435,18 @@ def get_tool_info_command(compression):
     return tool_info_command
 
 
+def get_7z_version():
+    return [
+        line
+        for line in subprocess.check_output("7z").splitlines()
+        if b"Version" in line
+    ][0].decode("utf8")
+
+
+def get_tar_version():
+    return subprocess.check_output(["tar", "--version"]).splitlines()[0].decode("utf8")
+
+
 def get_compression_event_detail(compression):
     """Return details of compression
 
@@ -442,17 +455,13 @@ def get_compression_event_detail(compression):
     """
     if compression in (COMPRESSION_7Z_BZIP, COMPRESSION_7Z_LZMA, COMPRESSION_7Z_COPY):
         try:
-            version = [
-                line
-                for line in subprocess.check_output("7z").splitlines()
-                if "Version" in line
-            ][0]
+            version = get_7z_version()
             event_detail = 'program="7z"; version="{}"'.format(version)
         except (subprocess.CalledProcessError, Exception):
             event_detail = 'program="7z"'
     elif compression in (COMPRESSION_TAR_BZIP2, COMPRESSION_TAR, COMPRESSION_TAR_GZIP):
         try:
-            version = subprocess.check_output(["tar", "--version"]).splitlines()[0]
+            version = get_tar_version()
             event_detail = 'program="tar"; version="{}"'.format(version)
         except (subprocess.CalledProcessError, Exception):
             event_detail = 'program="tar"'
@@ -490,9 +499,7 @@ def get_compression_transforms(aip, compression, transform_order):
                 "type": DECOMPRESS_TRANSFORM_TYPE,
             }
         )
-        version = [
-            x for x in subprocess.check_output("7z").splitlines() if "Version" in x
-        ][0]
+        version = get_7z_version()
         extension = COMPRESS_EXTENSION_7Z
         program_name = "7-Zip"
 
@@ -514,7 +521,7 @@ def get_compression_transforms(aip, compression, transform_order):
                 "type": DECOMPRESS_TRANSFORM_TYPE,
             }
         )
-        version = subprocess.check_output(["tar", "--version"]).splitlines()[0]
+        version = get_tar_version()
         extension = COMPRESS_EXTENSION_BZIP2
         program_name = "tar"
 
@@ -534,7 +541,7 @@ def get_compression_transforms(aip, compression, transform_order):
                 "type": DECOMPRESS_TRANSFORM_TYPE,
             }
         )
-        version = subprocess.check_output(["tar", "--version"]).splitlines()[0]
+        version = get_tar_version()
         extension = COMPRESS_EXTENSION_GZIP
         program_name = "tar"
 
