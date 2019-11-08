37c9321ed0c57b19686d1b01fb9008873aeb9b14
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2019-11-08 10:34:09 +0000

    Override jwk handling to make alg optional

--- storage_service/common/backends.py
+++ storage_service/common/backends.py
@@ -3,10 +3,14 @@ from __future__ import absolute_import
 
 import json
 
+import requests
 from django.conf import settings
 from django_cas_ng.backends import CASBackend
-from josepy.jws import JWS
+from django.core.exceptions import SuspiciousOperation
+from django.utils.encoding import smart_text
+from josepy.jws import JWS, Header
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
+from mozilla_django_oidc.utils import import_from_settings
 
 
 class CustomCASBackend(CASBackend):
@@ -24,6 +28,41 @@ class CustomOIDCBackend(OIDCAuthenticationBackend):
     """
     Provide OpenID Connect authentication
     """
+
+    def retrieve_matching_jwk(self, token):
+        """Get the signing key by exploring the JWKS endpoint of the OP."""
+
+        # This method is overridden to provide a fix for "alg" potentially not
+        # being present in the response (it is optional in the spec, and not
+        # provided by Azure)
+
+        # The latest version of mozilla-django-oidc provides this fix, but we
+        # cannot currently use it due to being on Django 1.8. Once we are on a
+        # recent Django version and using mozilla_django_oidc>=1.1.0 then we
+        # can discard this override.
+        response_jwks = requests.get(
+            self.OIDC_OP_JWKS_ENDPOINT,
+            verify=import_from_settings("OIDC_VERIFY_SSL", True),
+        )
+        response_jwks.raise_for_status()
+        jwks = response_jwks.json()
+
+        # Compute the current header from the given token to find a match
+        jws = JWS.from_compact(token)
+        json_header = jws.signature.protected
+        header = Header.json_loads(json_header)
+
+        key = None
+        for jwk in jwks["keys"]:
+            if jwk["kid"] != smart_text(header.kid):
+                continue
+            if "alg" in jwk and jwk["alg"] != smart_text(header.alg):
+                raise SuspiciousOperation("alg values do not match.")
+            key = jwk
+        if key is None:
+            raise SuspiciousOperation("Could not find a valid JWKS.")
+        return key
+
     def get_userinfo(self, access_token, id_token, verified_id):
         """
         Extract user details from JSON web tokens
