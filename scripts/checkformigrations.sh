a951058de738b7618a25313387a7afd4a401503e
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2021-01-21 00:10:53 -0600

    Use absolute paths in checkformigrations script

--- scripts/checkformigrations.sh
+++ scripts/checkformigrations.sh
@@ -3,12 +3,15 @@
 set -o errexit
 set -o pipefail
 
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
+
 function count_migrations() {
-    ls storage_service/locations/migrations/*.py | wc -l
+    ls ${__root}/storage_service/locations/migrations/*.py | wc -l
 }
 
 MIGRATIONS_COUNT_BEFORE=$(count_migrations)
-storage_service/manage.py makemigrations
+${__root}/storage_service/manage.py makemigrations
 MIGRATIONS_COUNT_AFTER=$(count_migrations)
 
 if [ $MIGRATIONS_COUNT_BEFORE -ne $MIGRATIONS_COUNT_AFTER ]; then
