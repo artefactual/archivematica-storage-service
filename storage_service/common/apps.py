f11f482688d6374a7771c40ce48f4f743cc98b9b
Jesús García Crespo <jesus@sevein.com>
2021-12-20 12:52:16 +0100

    Include application version info metric

--- storage_service/common/apps.py
+++ storage_service/common/apps.py
@@ -2,6 +2,12 @@
 from __future__ import absolute_import
 
 from django.apps import AppConfig
+from prometheus_client import Info
+
+from storage_service import __version__
+
+
+version_info = Info("version", "Archivematica Storage Service version info")
 
 
 class CommonAppConfig(AppConfig):
@@ -9,3 +15,4 @@ class CommonAppConfig(AppConfig):
 
     def ready(self):
         import common.signals  # noqa: F401
+        version_info.info({"version": __version__})
