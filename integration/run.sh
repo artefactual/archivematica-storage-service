ac4c39fcae303e2bd5f89809235a77b281b51ea6
Douglas Cerna <douglascerna@yahoo.com>
2023-05-04 12:05:44 -0600

    Update GitHub test workflow
    This also sets the exit code of the test run in the Compose integration test
    so it is reflected in the GitHub workflow job.

--- integration/run.sh
+++ integration/run.sh
@@ -4,8 +4,12 @@ __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cd ${__dir}
 
-env COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build minio mysql archivematica-storage-service
+env COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose build archivematica-storage-service
 
 docker-compose run --rm archivematica-storage-service
 
+status=$?
+
 docker-compose down --volumes
+
+exit $status
