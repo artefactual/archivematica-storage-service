c0b963cbdae4908209ae780f00f935c68768c71f
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2023-06-02 16:43:02 +0000

    Upgrade MySQL and MinIO in the integration test

--- storage_service/storage_service/settings/testmysql.py
+++ storage_service/storage_service/settings/testmysql.py
@@ -6,3 +6,4 @@ from .test import *
 DATABASES["default"] = dj_database_url.parse(
     "mysql://archivematica:demo@mysql/SSTEST", conn_max_age=600
 )
+DATABASES["default"]["TEST"] = {"NAME": "test_SSTEST"}
