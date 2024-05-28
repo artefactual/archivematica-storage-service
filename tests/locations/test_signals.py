31dbbb2682dd939a6c880737a1c3b26db6fc0678
Dhwani Patel <dpatel@artefactual.com>
2024-05-28 09:08:47 -0600

    Filter active users to send email notifications
    Fixity failure send reports to users that are "Active" and avoids emailing those marked "Inactive" by the SS admin

--- tests/locations/test_signals.py
+++ tests/locations/test_signals.py
@@ -3,14 +3,15 @@ import json
 from unittest import mock
 
 import pytest
+from django.contrib.auth.models import User
 from django.utils import timezone
 from locations import models
 from locations import signals
 
 
 @pytest.mark.django_db
-@mock.patch("locations.signals._notify_administrators")
-def test_report_failed_fixity_check(_notify_administrators: mock.Mock):
+@mock.patch("locations.signals._notify_users")
+def test_report_failed_fixity_check(_notify_users: mock.Mock):
     package = models.Package.objects.create(
         current_location=models.Location.objects.create(
             space=models.Space.objects.create()
@@ -37,9 +38,10 @@ def test_report_failed_fixity_check(_notify_administrators: mock.Mock):
     ):
         signals.report_failed_fixity_check(sender, **kwargs)
 
-    _notify_administrators.assert_called_once_with(
+    _notify_users.assert_called_once_with(
         f"Fixity check failed for package {package.uuid}",
         f"\n[{expected_time.strftime('%Y-%m-%d %H:%M:%S')}] A fixity check failed for the package with UUID {package.uuid}.\n",
+        mock.ANY,
     )
 
     assert (
@@ -48,3 +50,23 @@ def test_report_failed_fixity_check(_notify_administrators: mock.Mock):
         ).count()
         == 1
     )
+
+
+@pytest.fixture
+def user(db):
+    return User.objects.create_user(
+        username="demo", email="demo@example.com", password="Abc.Def.1234"
+    )
+
+
+@pytest.mark.django_db
+def test_notify_users(mailoutbox, user):
+    subject = "foo"
+    message = "bar"
+
+    signals._notify_users(subject, message, [user])
+
+    result = mailoutbox[0]
+    assert list(result.to) == [user.email]
+    assert result.subject == subject
+    assert result.body == message
