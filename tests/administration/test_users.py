84239fefb1dc7d14683446c071f2270bc574c673
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2024-11-06 16:10:34 +0000

    Convert user tests into pytest tests

--- tests/administration/test_users.py
+++ tests/administration/test_users.py
@@ -1,125 +1,136 @@
-from unittest import skipIf
+from typing import Type
 
+import pytest
+import pytest_django
 from administration import roles
-from django.conf import settings
 from django.contrib.auth.models import User
-from django.test import TestCase
+from django.test import Client
 from django.urls import reverse
 
 
-class TestUserManagement(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username="admin",
-            password="admin",
-            email="admin@example.com",
-            is_superuser=True,
-        )
-        self.client.login(username="admin", password="admin")
+def as_reader(user: User) -> None:
+    user.set_role(roles.USER_ROLE_READER)
 
-    def as_reader(self):
-        self.user.set_role(roles.USER_ROLE_READER)
 
-    def as_manager(self):
-        self.user.set_role(roles.USER_ROLE_MANAGER)
+def as_manager(user: User) -> None:
+    user.set_role(roles.USER_ROLE_MANAGER)
 
-    def test_list_users(self):
-        """The user list is available to all users."""
-        resp = self.client.get(reverse("administration:user_list"))
 
-        self.assertContains(resp, "<td>admin@example.com</td>")
+@pytest.mark.django_db
+def test_list_users(admin_client: Client) -> None:
+    """The user list is available to all users."""
+    resp = admin_client.get(reverse("administration:user_list"))
 
-    @skipIf(not settings.ALLOW_USER_EDITS, "User edits are disabled")
-    def test_create_user_as_admin(self):
-        """Only administrators are allowed to create new users."""
-        resp = self.client.post(
-            reverse("administration:user_create"),
-            {
-                "username": "demo",
-                "email": "demo@example.com",
-                "role": "manager",
-                "password1": "ck61Qc873.KxoZ5G",
-                "password2": "ck61Qc873.KxoZ5G",
-            },
-        )
+    assert "<td>admin@example.com</td>" in resp.content.decode()
 
-        self.assertRedirects(
-            resp,
-            reverse("administration:user_list"),
-            status_code=302,
-            target_status_code=200,
-            fetch_redirect_response=True,
-        )
-        assert User.objects.filter(username="demo").exists() is True
 
-    @skipIf(not settings.ALLOW_USER_EDITS, "User edits are disabled")
-    def test_create_user_as_non_admin(self):
-        """Only administrators are allowed to create new users."""
-        self.as_reader()
-        resp = self.client.post(
-            reverse("administration:user_create"),
-            {
-                "username": "demo",
-                "email": "demo@example.com",
-                "role": "manager",
-                "password1": "ck61Qc873.KxoZ5G",
-                "password2": "ck61Qc873.KxoZ5G",
-            },
-        )
+@pytest.fixture
+def settings(
+    settings: pytest_django.fixtures.SettingsWrapper,
+) -> pytest_django.fixtures.SettingsWrapper:
+    settings.ALLOW_USER_EDITS = True
 
-        self.assertRedirects(
-            resp,
-            reverse("administration:user_list"),
-            status_code=302,
-            target_status_code=200,
-            fetch_redirect_response=True,
-        )
-        assert User.objects.filter(username="demo").exists() is False
+    return settings
 
-    @skipIf(not settings.ALLOW_USER_EDITS, "User edits are disabled")
-    def test_edit_user_promote_as_manager(self):
-        """Only administrators are allowed to promote/demote users."""
-        test = User.objects.create_user(
-            username="test", password="ck61Qc873.KxoZ5G", email="test@example.com"
-        )
-        resp = self.client.post(
-            reverse("administration:user_edit", kwargs={"id": test.pk}),
-            {
-                "user": "Edit User",
-                "username": "test",
-                "email": "test@example.com",
-                "role": "manager",
-            },
-            follow=True,
-        )
 
-        assert list(resp.context["messages"])[0].message == "User information saved."
-        test.refresh_from_db()
-        assert test.get_role() == roles.USER_ROLE_MANAGER
+@pytest.mark.django_db
+def test_create_user_as_admin(
+    admin_client: Client, settings: pytest_django.fixtures.SettingsWrapper
+) -> None:
+    """Only administrators are allowed to create new users."""
+    resp = admin_client.post(
+        reverse("administration:user_create"),
+        {
+            "username": "demo",
+            "email": "demo@example.com",
+            "role": "manager",
+            "password1": "ck61Qc873.KxoZ5G",
+            "password2": "ck61Qc873.KxoZ5G",
+        },
+        follow=True,
+    )
+    assert resp.status_code == 200
 
-    @skipIf(not settings.ALLOW_USER_EDITS, "User edits are disabled")
-    def test_edit_user_promotion_requires_admin(self):
-        """Only administrators are allowed to promote/demote users."""
-        self.as_manager()
-        test = User.objects.create_user(
-            username="test", password="ck61Qc873.KxoZ5G", email="test@example.com"
-        )
-        resp = self.client.post(
-            reverse("administration:user_edit", kwargs={"id": test.pk}),
-            {
-                "user": "Edit User",
-                "username": "test",
-                "email": "test@example.com",
-                "role": "manager",
-            },
-        )
+    assert "<td>demo@example.com</td>" in resp.content.decode()
+    assert User.objects.filter(username="demo").exists()
 
-        self.assertRedirects(
-            resp,
-            reverse("administration:user_list"),
-            status_code=302,
-            target_status_code=200,
-            fetch_redirect_response=True,
-        )
-        test.refresh_from_db()
-        assert test.get_role() == roles.USER_ROLE_READER
+
+@pytest.mark.django_db
+def test_create_user_as_non_admin(
+    admin_client: Client,
+    settings: pytest_django.fixtures.SettingsWrapper,
+    django_user_model: Type[User],
+) -> None:
+    """Only administrators are allowed to create new users."""
+    as_reader(django_user_model.objects.get(username="admin"))
+
+    resp = admin_client.post(
+        reverse("administration:user_create"),
+        {
+            "username": "demo",
+            "email": "demo@example.com",
+            "role": "manager",
+            "password1": "ck61Qc873.KxoZ5G",
+            "password2": "ck61Qc873.KxoZ5G",
+        },
+        follow=True,
+    )
+    assert resp.status_code == 200
+
+    assert "<td>demo@example.com</td>" not in resp.content.decode()
+    assert not User.objects.filter(username="demo").exists()
+
+
+@pytest.mark.django_db
+def test_edit_user_promote_as_manager(
+    admin_client: Client,
+    settings: pytest_django.fixtures.SettingsWrapper,
+    django_user_model: Type[User],
+) -> None:
+    """Only administrators are allowed to promote/demote users."""
+    test = django_user_model.objects.create_user(
+        username="test", password="ck61Qc873.KxoZ5G", email="test@example.com"
+    )
+    resp = admin_client.post(
+        reverse("administration:user_edit", kwargs={"id": test.pk}),
+        {
+            "user": "Edit User",
+            "username": "test",
+            "email": "test@example.com",
+            "role": "manager",
+        },
+        follow=True,
+    )
+    assert resp.status_code == 200
+
+    assert list(resp.context["messages"])[0].message == "User information saved."
+    test.refresh_from_db()
+    assert test.get_role() == roles.USER_ROLE_MANAGER
+
+
+@pytest.mark.django_db
+def test_edit_user_promotion_requires_admin(
+    admin_client: Client,
+    settings: pytest_django.fixtures.SettingsWrapper,
+    django_user_model: Type[User],
+) -> None:
+    """Only administrators are allowed to promote/demote users."""
+    as_manager(django_user_model.objects.get(username="admin"))
+    test = django_user_model.objects.create_user(
+        username="test", password="ck61Qc873.KxoZ5G", email="test@example.com"
+    )
+
+    resp = admin_client.post(
+        reverse("administration:user_edit", kwargs={"id": test.pk}),
+        {
+            "user": "Edit User",
+            "username": "test",
+            "email": "test@example.com",
+            "role": "manager",
+        },
+        follow=True,
+    )
+    assert resp.status_code == 200
+
+    test.refresh_from_db()
+    assert test.get_role() == roles.USER_ROLE_READER
