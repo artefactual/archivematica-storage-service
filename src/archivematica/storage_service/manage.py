ad06dde1e2c8545d72127094f237d0bc7993ad56
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-03-20 20:26:00 +0000

    Update manage script

--- src/archivematica/storage_service/manage.py
+++ src/archivematica/storage_service/manage.py
@@ -3,7 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storage_service.settings.local")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "archivematica.storage_service.storage_service.settings.local",
+    )
 
     from django.core.management import execute_from_command_line
 
