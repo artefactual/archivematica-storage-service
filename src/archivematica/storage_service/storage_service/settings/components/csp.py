b752233b712be99ae33c3d3f36fcc65549349d2f
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2025-08-30 17:31:45 +0000

    Migrate directives in CSP settings file

--- src/archivematica/storage_service/storage_service/settings/components/csp.py
+++ src/archivematica/storage_service/storage_service/settings/components/csp.py
@@ -1,7 +1,15 @@
-CSP_DEFAULT_SRC = ["'none'"]
-CSP_SCRIPT_SRC = ["'self'", "'unsafe-inline'", "'unsafe-eval'"]
-CSP_STYLE_SRC = ["'self'", "'unsafe-inline'"]
-CSP_IMG_SRC = ["'self'"]
+from csp.constants import NONE
+from csp.constants import SELF
+from csp.constants import UNSAFE_EVAL
+from csp.constants import UNSAFE_INLINE
 
-# for the create space form
-CSP_CONNECT_SRC = ["'self'"]
+CONTENT_SECURITY_POLICY = {
+    "DIRECTIVES": {
+        "default-src": [NONE],
+        "script-src": [SELF, UNSAFE_INLINE, UNSAFE_EVAL],
+        "style-src": [SELF, UNSAFE_INLINE],
+        "img-src": [SELF],
+        # for the create space form
+        "connect-src": [SELF],
+    }
+}
